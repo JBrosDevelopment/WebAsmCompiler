838e61901d784a6c2ca8d408c441d8de6717f539
Joseph H <jlhamilton0408@gmail.com>
2026-09-05 21:13:13 +0000

    refactored project

---
+++ pseudo-testing/FullAssembler.py
@@ -0,0 +1,30 @@
+memory = """
+
+""" + str(0xFE)
+
+
+def print_i32(value):
+    print(value)
+
+def write_char(char):
+    print('write: `', char, '`')
+
+def writefromHex(index: int) -> int:
+    pass
+
+def writeFromBin(index: int) -> int:
+    pass
+
+def writeFromUleb(index: int) -> int:
+    pass
+
+def writeFromString(index: int) -> int:
+    pass
+
+def writeByte(index: int) -> int:
+    pass
+
+def main():
+    pass
+
+main()
